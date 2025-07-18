beb178470d7c657d31ca39e9939e40ccb6ed996a
Abeesh A I <60069298+abeeshai96@users.noreply.github.com>
2025-07-18 11:13:43 +0000

    Added API Call Sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppService } from './app.service';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'CodeSandbox';
+  constructor(private appService: AppService) {}
+
+  title = 'Angular Boilerplate';
+  jsonData!: string;
+
+  fetchData() {
+    this.appService.getData().subscribe({
+      next: (response) => {
+        this.jsonData = JSON.stringify(response);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
